aa3bc77bbafe61fc1877ee0cbc1573dd792d3b6c
Charles Wang <zwcharl@gmail.com>
2025-10-13 22:04:09 -0400

    format: using prettier

--- src/stage/stage.ts
+++ src/stage/stage.ts
@@ -3,15 +3,15 @@ import { Lights } from "./lights";
 import { Scene } from "./scene";
 
 export class Stage {
-    scene: Scene;
-    lights: Lights;
-    camera: Camera;
-    stats: Stats;
+  scene: Scene;
+  lights: Lights;
+  camera: Camera;
+  stats: Stats;
 
-    constructor(scene: Scene, lights: Lights, camera: Camera, stats: Stats) {
-        this.scene = scene;
-        this.lights = lights;
-        this.camera = camera;
-        this.stats = stats;
-    }
+  constructor(scene: Scene, lights: Lights, camera: Camera, stats: Stats) {
+    this.scene = scene;
+    this.lights = lights;
+    this.camera = camera;
+    this.stats = stats;
+  }
 }
