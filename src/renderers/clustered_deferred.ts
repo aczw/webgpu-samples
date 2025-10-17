766e1908754f2c27d5f875fedba4d2791b08053d
Charles Wang <zwcharl@gmail.com>
2025-10-17 14:46:01 -0400

    deferred: copy over F+ code

--- src/renderers/clustered_deferred.ts
+++ src/renderers/clustered_deferred.ts
@@ -1,22 +1,156 @@
-import * as renderer from "../renderer";
-import * as shaders from "../shaders/shaders";
+import {
+  Renderer,
+  device,
+  modelBindGroupLayout,
+  materialBindGroupLayout,
+  vertexBufferLayout,
+  canvasFormat,
+  context,
+  canvas,
+} from "../renderer";
+import { naiveVertSrc, constants, clusteredDeferredFullscreenFragSrc } from "../shaders/shaders";
 import { Stage } from "../stage/stage";
 
-export class ClusteredDeferredRenderer extends renderer.Renderer {
-  // TODO-3: add layouts, pipelines, textures, etc. needed for Forward+ here
-  // you may need extra uniforms such as the camera view matrix and the canvas resolution
+export class ClusteredDeferredRenderer extends Renderer {
+  sceneUniformsBindGroupLayout: GPUBindGroupLayout;
+  sceneUniformsBindGroup: GPUBindGroup;
+
+  depthTexture: GPUTexture;
+
+  pipeline: GPURenderPipeline;
 
   constructor(stage: Stage) {
     super(stage);
 
-    // TODO-3: initialize layouts, pipelines, textures, etc. needed for Forward+ here
-    // you'll need two pipelines: one for the G-buffer pass and one for the fullscreen pass
+    this.sceneUniformsBindGroupLayout = device.createBindGroupLayout({
+      label: "Deferred scene uniforms bind group layout",
+      entries: [
+        {
+          // Camera uniforms
+          binding: 0,
+          visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
+          buffer: { type: "uniform" },
+        },
+        {
+          // Light set
+          binding: 1,
+          visibility: GPUShaderStage.FRAGMENT,
+          buffer: { type: "read-only-storage" },
+        },
+        {
+          // Cluster set storage buffer
+          binding: 2,
+          visibility: GPUShaderStage.FRAGMENT,
+          buffer: { type: "read-only-storage" },
+        },
+        {
+          // Num slices uniform buffer
+          binding: 3,
+          visibility: GPUShaderStage.FRAGMENT,
+          buffer: { type: "uniform" },
+        },
+        {
+          // Dimensions uniform buffer
+          binding: 4,
+          visibility: GPUShaderStage.FRAGMENT,
+          buffer: { type: "uniform" },
+        },
+      ],
+    });
+
+    this.sceneUniformsBindGroup = device.createBindGroup({
+      label: "Deferred scene uniforms bind group",
+      layout: this.sceneUniformsBindGroupLayout,
+      entries: [
+        { binding: 0, resource: { buffer: this.camera.uniformsBuffer } },
+        { binding: 1, resource: { buffer: this.lights.lightSetStorageBuffer } },
+        { binding: 2, resource: { buffer: this.lights.clusterSetStorageBuffer } },
+        { binding: 3, resource: { buffer: this.lights.numSlicesUniformBuffer } },
+        { binding: 4, resource: { buffer: this.lights.dimensionsUniformBuffer } },
+      ],
+    });
+
+    this.depthTexture = device.createTexture({
+      size: [canvas.width, canvas.height],
+      format: "depth24plus",
+      usage: GPUTextureUsage.RENDER_ATTACHMENT,
+    });
+
+    this.pipeline = device.createRenderPipeline({
+      label: "Deferred render pipeline",
+      layout: device.createPipelineLayout({
+        label: "Deferred render pipeline layout",
+        bindGroupLayouts: [
+          this.sceneUniformsBindGroupLayout,
+          modelBindGroupLayout,
+          materialBindGroupLayout,
+        ],
+      }),
+      depthStencil: {
+        depthWriteEnabled: true,
+        depthCompare: "less",
+        format: "depth24plus",
+      },
+      vertex: {
+        module: device.createShaderModule({
+          label: "Deferred vertex shader (same as naive)",
+          code: naiveVertSrc,
+        }),
+        buffers: [vertexBufferLayout],
+      },
+      fragment: {
+        module: device.createShaderModule({
+          label: "Deferred fullscreen fragment shader",
+          code: clusteredDeferredFullscreenFragSrc,
+        }),
+        targets: [
+          {
+            format: canvasFormat,
+          },
+        ],
+      },
+    });
   }
 
   override draw() {
-    // TODO-3: run the Forward+ rendering pass:
-    // - run the clustering compute shader
-    // - run the G-buffer pass, outputting position, albedo, and normals
-    // - run the fullscreen pass, which reads from the G-buffer and performs lighting calculations
+    const encoder = device.createCommandEncoder();
+    const canvasTextureView = context.getCurrentTexture().createView();
+
+    this.lights.doLightClustering(encoder);
+
+    const renderPass = encoder.beginRenderPass({
+      label: "Deferred render pass",
+      colorAttachments: [
+        {
+          view: canvasTextureView,
+          clearValue: [0, 0, 0, 0],
+          loadOp: "clear",
+          storeOp: "store",
+        },
+      ],
+      depthStencilAttachment: {
+        view: this.depthTexture.createView(),
+        depthClearValue: 1.0,
+        depthLoadOp: "clear",
+        depthStoreOp: "store",
+      },
+    });
+    renderPass.setPipeline(this.pipeline);
+
+    renderPass.setBindGroup(constants.bindGroup_scene, this.sceneUniformsBindGroup);
+
+    this.scene.iterate(
+      (node) => renderPass.setBindGroup(constants.bindGroup_model, node.modelBindGroup),
+      (material) =>
+        renderPass.setBindGroup(constants.bindGroup_material, material.materialBindGroup),
+      (primitive) => {
+        renderPass.setVertexBuffer(0, primitive.vertexBuffer);
+        renderPass.setIndexBuffer(primitive.indexBuffer, "uint32");
+        renderPass.drawIndexed(primitive.numIndices);
+      }
+    );
+
+    renderPass.end();
+    device.queue.submit([encoder.finish()]);
   }
 }
--- src/shaders/clustered_deferred_fullscreen.fs.wgsl
+++ src/shaders/clustered_deferred_fullscreen.fs.wgsl
@@ -1,3 +1,58 @@
-// TODO-3: implement the Clustered Deferred fullscreen fragment shader
-
 // Similar to the Forward+ fragment shader, but with vertex information coming from the G-buffer instead.
+
+@group(${bindGroup_scene}) @binding(0) var<uniform> camera: CameraUniforms;
+@group(${bindGroup_scene}) @binding(1) var<storage, read> lightSet: LightSet;
+@group(${bindGroup_scene}) @binding(2) var<storage, read> clusterSet: ClusterSet;
+@group(${bindGroup_scene}) @binding(3) var<uniform> numSlices: vec3<u32>;
+@group(${bindGroup_scene}) @binding(4) var<uniform> dimensions: vec2<u32>;
+
+@group(${bindGroup_material}) @binding(0) var diffuseTex: texture_2d<f32>;
+@group(${bindGroup_material}) @binding(1) var diffuseTexSampler: sampler;
+
+struct FragmentInput
+{
+    @builtin(position) pixel: vec4<f32>,
+    @location(0) pos: vec3f,
+    @location(1) nor: vec3f,
+    @location(2) uv: vec2f
+}
+
+@fragment
+fn main(in: FragmentInput) -> @location(0) vec4f
+{
+    let diffuseColor = textureSample(diffuseTex, diffuseTexSampler, in.uv);
+    
+    if (diffuseColor.a < 0.5) {
+        discard;
+    }
+
+    let clip: vec4<f32> = camera.viewProjection * vec4<f32>(in.pos, 1.0);
+    let ndc: vec3<f32> = clip.xyz / clip.w;
+    let uv: vec2<f32> = (ndc.xy + 1.0) * 0.5;
+
+    // Truncate to find cluster indices
+    let clusterX = u32(uv.x * f32(dimensions.x) / f32(${clusterPixelWidth}));
+    let clusterY = u32(uv.y * f32(dimensions.y) / f32(${clusterPixelHeight}));
+
+    // Since our depth slices aren't linearly spaced, we have to derive it. And since
+    // we did the original z-value calculations in view space, we do the same here
+    let viewPos: vec4<f32> = camera.view * vec4<f32>(in.pos, 1.0);
+    let logFarNearInv: f32 = 1.0 / log(camera.farPlane / camera.nearPlane);
+    let clusterZ = u32(
+        (log(-viewPos.z) * f32(numSlices.z) * logFarNearInv) -
+        (f32(numSlices.z) * log(camera.nearPlane) * logFarNearInv)
+    );
+
+    let index = (clusterZ * numSlices.x * numSlices.y) + (clusterY * numSlices.x) + clusterX;
+    var numLights = clusterSet.clusters[index].numLights;
+    var lightSum = vec3<f32>();
+
+    for (var clusterLightIndex = 0u; clusterLightIndex < numLights; clusterLightIndex++) {
+        let lightIndex = clusterSet.clusters[index].lights[clusterLightIndex];
+        let light = lightSet.lights[lightIndex];
+        lightSum += calculateLightContrib(light, in.pos, normalize(in.nor));
+    }
+
+    let finalColor = diffuseColor.rgb * lightSum;
+    return vec4<f32>(finalColor, 1.0);
+}
