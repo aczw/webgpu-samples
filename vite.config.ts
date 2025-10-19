e1b696dad70e6091f46fdbddceac0dbe2861bfa1
Charles Wang <zwcharl@gmail.com>
2025-10-18 20:58:34 -0400

    vite: configure for MPA build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const config = defineConfig({
   build: {
     target: "esnext",
+    rollupOptions: {
+      input: {
+        root: resolve(__dirname, "index.html"),
+        "forward-plus-clustered-deferred": resolve(
+          __dirname,
+          "forward-plus-clustered-deferred/index.html"
+        ),
+      },
+    },
   },
 });
 
